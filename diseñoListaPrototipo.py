c6c0089fe768f11de1e63233cbd08fe240b93082
jbpixel52 <50649423+jbpixel52@users.noreply.github.com>
2020-05-14 13:47:07 -0700

    agregue el constructor del app

--- diseñoListaPrototipo.py
+++ diseñoListaPrototipo.py
@@ -4,27 +4,37 @@ import tkinter.font as tkFont
 from json import *
 
 
+'''RECUERDEN COMENTAR SUS CAMBIOS'''
+
 screenSize = {'width': 360, 'height': 800}
-
-
 root = tk.Tk()
-root.title('root')
 root.minsize(screenSize['width'], screenSize['height'])
 root.resizable(False, False)
 
-seccion1 = tk.Frame(master=root, bg="green")
-seccion1.place(relwidth=1.0, relheight=0.1, anchor=tk.NW)
-seccion1.place_configure(relx=0, rely=0)
 
-label1 = tk.Label(master=seccion1,
-                  bg="green",
-                  fg="white",
-                  text="Lista de memes",
-                  font=tkFont.Font(family='Roboto', size=20)
-                  )
-label1.place(anchor=tk.NW, relx=0.1, rely=0.2)
+class app:
+    def __init__(self, master, titulo):
+        self.screen = tk.Frame(master)
+        self.screen.place(height=640, width=360, anchor=tk.NW)
+        self.titulo = titulo
+        root.title(self.titulo)
+        self.encabezado = tk.Frame(self.screen, bg="lime green")
+        self.encabezado.place(relwidth=1.0, relheight=0.1, anchor=tk.NW)
+        self.encabezado.place_configure(relx=0, rely=0)
 
-seccion2 = tk.Frame(master=root, bg="white")
-seccion2.place(anchor=tk.NW, relwidth=1.0, relheight=0.9, relx=0, rely=0.1)
+        self.label_titulo = tk.Label(self.encabezado,
+                                     bg="lime green",
+                                     fg="white",
+                                     text=self.titulo,
+                                     font=tkFont.Font(family='Roboto', size=20)
+                                     )
+        self.label_titulo.place(anchor=tk.NW, relx=0.1, rely=0.2)
+
+        self.cuerpo = tk.Frame(self.screen, bg="white")
+        self.cuerpo.place(anchor=tk.NW, relwidth=1.0,
+                          relheight=0.9, relx=0, rely=0.1)
+
+
+lista_memes = app(root, 'LISTA DE MEMES')
 
 root.mainloop()
